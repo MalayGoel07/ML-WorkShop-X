691bf91742686f1b6aefae84dbc63aa65349460a
Malay Goel <malaygoel07@gmail.com>
2026-08-19 08:42:12 +0530

    updated databse

--- backend/db.py
+++ backend/db.py
@@ -12,5 +12,5 @@ cluster = os.getenv("MONGO_CLUSTER")
 MONGO_URI = f"mongodb+srv://{quote_plus(username)}:{quote_plus(password)}@{cluster}/"
 
 client = MongoClient(MONGO_URI)
-db = client["qbot_x"]
-users_collection = db["users"]
+db = client["ml_workshop"]
+users_collection = db["user"]
